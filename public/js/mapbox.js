e62c33f5e1d41eaa5c44866ee34b34392b0f62d4
unknown <clark.ny@gmail.com>
2020-06-13 07:56:02 -0400

    Test

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -4,12 +4,24 @@ const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNiY2xhcmsiLCJhIjoiY2tiYjFwenJ1MDAzeDJ2cWh3ajh2Zmd2dSJ9.ZadTq3QiGvrica-K1luvPQ';
 
+
+
 var map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/chrisbclark/ckbb2ourn08de1iqiy7o743ic',
+//scrollZoom: 'false',
+showZoom: 'true'
 // center: [-80.743814,34.925885],
 // zoom: 15
 });
+
+map.addControl(new mapboxgl.GeolocateControl({
+  positionOptions: {
+  enableHighAccuracy: true
+  },
+  trackUserLocation: true,
+  }));
+
 
 const bounds = new mapboxgl.LngLatBounds();
 
@@ -23,7 +35,23 @@ locations.forEach(loc => {
         element: el,
         anchor: 'bottom'
     }).setLngLat(loc.coordinates).addTo(map);
-//Extend the bounds to include the current location
+
+
+    // Add a pop up
+
+    new mapboxgl.Popup({
+      offset: 30
+    }).setLngLat(loc.coordinates).setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`).addTo(map)
+
+
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      }));
+
+    //Extend the bounds to include the current location
 bounds.extend(loc.coordinates)
 map.fitBounds(bounds, {
     padding: {
@@ -34,3 +62,4 @@ map.fitBounds(bounds, {
     }
   })
 })
+console.log('Hello!')
